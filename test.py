53c34abe9d66e96ce91ade968c488f7f66d4c235
Moinalee <moinaleeahmad@gmail.com.com>
2020-05-02 12:34:05 +0530

    sending base64 to server and receving gray scale image

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import cv2
 import imutils
 from PIL import Image
 import numpy as np
+from threading import Thread
 
 app = Flask(__name__)
 #CORS(app, supports_credentials=True)
@@ -16,7 +17,7 @@ socketio = SocketIO(app)
 @app.route('/',methods=['POST', 'GET'])
 @cross_origin(supports_credentials=True)
 def index():
-    return render_template('index3.html')
+    return render_template('index.html')
 
 @socketio.on('catch-frame')
 def catch_frame(data):
@@ -30,20 +31,28 @@ def catch_frame(data):
 
 @socketio.on('image')
 def image(data_image):
-    print(data_image,"data_image")
+    #print("starting",data_image,"ending_data_image")
 
 
     # decode and convert into image
-
+    #print(type(data_image))
     encoded_data = data_image.split(',')[1]
     nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
     frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
+    #print(type(frame))
+    #frame1 = np.zeros(frame,np.uint8)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow('Original image',frame1)
+    #cv2.waitKey(5)
+    #cv2.imshow('Gray image', gray)
+    #print("just",frame,"now",gray)
+    #cv2.imwrite('color_img.jpg', gray)
+    #showing()
 
     # Process the image frame
     #frame = imutils.resize(frame, width=200)
-    frame = cv2.flip(frame, 1)
-    imgencode = cv2.imencode('.jpg', frame)[1]
+    gray = cv2.flip(gray, 1)
+    imgencode = cv2.imencode('.jpg', gray)[1]
 
     # base64 encode
     stringData = base64.b64encode(imgencode).decode('utf-8')
@@ -52,9 +61,14 @@ def image(data_image):
 
     # emit the frame back
     emit('response_back', stringData)
-    #print(stringData)
-
+    print(stringData)
+def showing():
+        just = cv2.imread('/color_img.jpg')
+        cv2.imshow('Gray',just)
+        cv2.waitKey(10)
+        print("here")
 if __name__ == '__main__':
     #context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     #context.load_cert_chain('server.crt', 'server.key')
     socketio.run(app,host='0.0.0.0',debug=True)#,ssl_context=context)
+    #Thread(target=showing).start()
